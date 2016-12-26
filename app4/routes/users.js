43e2a4d52fa503d669176e70723e50d75cb14c40
Tiago Mazzarollo <tmazza@email.com>
2016-12-26 20:32:03 -0200

    crud: add

---
+++ app4/routes/users.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../config/db.js');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var collection = db.get().collection('users');
+  
+  collection.find().toArray(function(err, users) {
+    console.log(users);
+    res.render('user/index', { users: users, toJs: JSON.stringify(users) });
+  });
+
+});
+
+/* GET add user. */
+router.get('/add', function(req, res, next) {
+  res.render('user/add');
+});
+
+router.post('/add', function(req, res, next) {
+
+  // TODO: usar validadores
+  var collection = db.get().collection('users');
+  collection.insert(req.body);
+
+  res.redirect('/users');
+
+});
+
+module.exports = router;
