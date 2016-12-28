e32d90125d417c275bef1fb5311b36bbed058ee7
Tiago Mazzarollo <tmazza@email.com>
2016-12-28 20:56:41 -0200

    busca de endereços

--- app7/components/filters.js
+++ app7/components/filters.js
@@ -1,9 +1,10 @@
 exports.mustLogin = function(req, res, next) {
-  if (req.session.user) {
     next();
-  } else {
-    req.session.error = 'Identifique-se!';
-    res.redirect('/login');
-  }
+  // if (req.session.user) {
+  //   next();
+  // } else {
+  //   req.session.error = 'Identifique-se!';
+  //   res.redirect('/login');
+  // }
 }
 
