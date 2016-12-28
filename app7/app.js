ef04b5de0de4f085e4acea7d528fe43f3c7f88d0
Tiago Mazzarollo <tmazza@email.com>
2016-12-28 18:13:32 -0200

    Login/logout

--- app7/app.js
+++ app7/app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var sensors = require('./routes/sensors');
 
 var app = express();
 
@@ -22,8 +23,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'shhhh, very secret'
+}));
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/sensors', sensors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app7/routes/index.js
+++ app7/routes/index.js
@@ -1,9 +1,81 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../components/db');
+var hash = require('../components/auth').hash;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    user: req.session.user, 
+  });
 });
+
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    msg: req.session.error,
+  });  
+});
+
+/* GET logout */
+router.get('/logout', function(req, res, next) {
+  req.session.destroy(function() {
+    res.redirect('/');
+  });
+});
+
+router.post('/login', function(req, res, next) {
+
+  authenticate(req.body.username, req.body.pwd, function(err, user){
+      if (user) {
+        req.session.regenerate(function(){
+          req.session.user = user;
+          res.redirect('/');
+        });
+      } else {
+        req.session.error = 'Usário ou senha incorreta. Use admin/admin';
+        res.redirect('/login');
+      }
+  });
+
+});
+
+var users = {
+  admin: { nome: 'admin' },
+};
+
+hash('admin', function(err, salt, hash){
+  if (err) throw err;
+  users.admin.salt = salt;
+  users.admin.hash = hash;
+});
+
+
+function authenticate(name, pass, fn) {
+  console.log("-----" + name);
+  console.log("-----" + pass);
+  if (!module.parent) console.log('authenticating %s:%s', name, pass);
+
+  var user = users[name];
+  if (!user) return fn(new Error('Usuário não encontrado'));
+
+  hash(pass, user.salt, function(err, hash){
+    if (err) return fn(err);
+    if (hash == user.hash) return fn(null, user);
+    fn(new Error('Usuário ou senha incorreta.'));
+  });
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Identifique-se!';
+    res.redirect('/login');
+  }
+}
+
+
 
 module.exports = router;
